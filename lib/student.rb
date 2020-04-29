130413ef089e4a4ff415eb280c2f6ce9f1222393
James Johnson <jtaylorjohnson@gmail.com>
2020-04-29 22:22:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,21 @@
 class Student
   
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
+  def initialize (name, album, id=nil)
+    @name = name
+    @album = album
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade )
+    SQL
   
 
   # Remember, you can access your database connection anywhere in this class
