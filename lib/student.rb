0981f10563e5b46c6e2da796522176488f5c6eac
James Johnson <jtaylorjohnson@gmail.com>
2020-04-29 22:30:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   
   def initialize (name, album, id=nil)
     @name = name
-    @album = album
+    @grade = grade
     @id = id
   end
   
@@ -21,10 +21,18 @@ class Student
   end
   
   def self.drop_table
-    sql = 
+    sql = << DROP TABLE students
+    DB[:conn].execute(sql)
+  end
   
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = 
+  end
+ 
   
 end
